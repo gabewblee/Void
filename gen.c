829d680b1698cc70d644a2afa63f58e1cbe33baf
gabewblee <gabe.wblee@gmail.com>
2026-09-17 02:38:06 +0900

    Feat: Arithmetic expressions implemented.

--- gen.c
+++ gen.c
@@ -1,26 +1,95 @@
+#include <stdlib.h>
+
 #include "gen.h"
 
 static void gen_expr(FILE *out, Expr *expr) {
-    /*
-     *   mov $expr->integer, %eax
-     */
-    fprintf(out, "  mov $%ld, %%eax\n", expr->integer);
+    switch (expr->type) {
+    case EXPR_INTEGER:
+        fprintf(out, "    movl $%ld, %%eax\n", expr->integer);
+        return;
+    case EXPR_UNARY:
+        gen_expr(out, expr->unary.operand);
+        switch (expr->unary.op) {
+        case TOKEN_PLUS:
+            return;
+        case TOKEN_MINUS:
+            fprintf(out, "    negl %%eax\n");
+            return;
+        default:
+            break;
+        }
+        break;
+    case EXPR_BINARY:
+        /*
+         *     gen_expr(out, expr->binary.left)
+         *     push %rax
+         *
+         *     gen_expr(out, expr->binary.right)
+         *     popq %rcx
+         */
+        gen_expr(out, expr->binary.left);
+        fprintf(out, "    pushq %%rax\n");
+
+        gen_expr(out, expr->binary.right);
+        fprintf(out, "    popq %%rcx\n");
+
+        switch (expr->binary.op) {
+        case TOKEN_PLUS:
+            /*
+             *    addl %ecx, %eax
+             */
+            fprintf(out, "    addl %%ecx, %%eax\n");
+            return;
+        case TOKEN_MINUS:
+            /*
+             *    subl %eax, %ecx
+             *    movl %ecx, %eax
+             */
+            fprintf(out, "    subl %%eax, %%ecx\n");
+            fprintf(out, "    movl %%ecx, %%eax\n");
+            return;
+        case TOKEN_STAR:
+            /*
+             * imull %ecx, %eax
+             */
+            fprintf(out, "    imull %%ecx, %%eax\n");
+            return;
+        case TOKEN_SLASH:
+            /*
+             *     movl %eax, %esi
+             *     movl %ecx, %eax
+             *     cltd
+             *     idivl %esi
+             */
+            fprintf(out, "    movl %%eax, %%esi\n");
+            fprintf(out, "    movl %%ecx, %%eax\n");
+            fprintf(out, "    cltd\n");
+            fprintf(out, "    idivl %%esi\n");
+            return;
+        default:
+            break;
+        }
+        break;
+    }
+
+    fprintf(stderr, "Error: Unsupported expression\n");
+    exit(EXIT_FAILURE);
 }
 
 static void gen_return_stmt(FILE *out, ReturnStmt *stmt) {
     /*
-     * gen_expr()
+     * gen_expr(out, stmt->expr)
      *   ret
      */
     gen_expr(out, stmt->expr);
-    fprintf(out, "  ret\n");
+    fprintf(out, "    ret\n");
 }
 
 static void gen_function(FILE *out, Function *function) {
     /*
      * .global function->name
      * function->name:
-     * gen_return_stmt()
+     * gen_return_stmt(out, function->body)
      */
     fprintf(out, ".global %s\n", function->name);
     fprintf(out, "%s:\n", function->name);
@@ -30,7 +99,7 @@ static void gen_function(FILE *out, Function *function) {
 void gen(FILE *out, Program *program) {
     /*
      * .text
-     * gen_function()
+     * gen_function(out, program->function)
      */
     fprintf(out, ".text\n");
     gen_function(out, program->function);
--- parser.c
+++ parser.c
@@ -4,6 +4,8 @@
 
 #include "parser.h"
 
+static Expr *parse_expr(Parser *parser);
+
 static void advance(Parser *parser) {
     parser->lookahead = lexer_get_nxt_token(parser->lexer);
 }
@@ -29,10 +31,58 @@ static char *copy_token_lexeme(Token token) {
     return lexeme;
 }
 
+static Expr *parse_primary(Parser *parser) {
+    if (parser->lookahead.type == TOKEN_NUMBER) {
+        long num = parser->lookahead.num;
+        match(parser, TOKEN_NUMBER);
+        return ast_build_integer_expr_node(num);
+    }
+
+    if (parser->lookahead.type == TOKEN_LPAREN) {
+        match(parser, TOKEN_LPAREN);
+        Expr *primary = parse_expr(parser);
+        match(parser, TOKEN_RPAREN);
+        return primary;
+    }
+
+    fprintf(stderr, "Error: Expected primary, but got '%d'\n", parser->lookahead.type);
+    exit(EXIT_FAILURE);
+}
+
+static Expr *parse_unary(Parser *parser) {
+    TokenType op = parser->lookahead.type;
+    if (op == TOKEN_PLUS || op == TOKEN_MINUS) {
+        match(parser, op);
+        return ast_build_unary_expr_node(op, parse_unary(parser));
+    }
+
+    return parse_primary(parser);
+}
+
+static Expr *parse_term(Parser *parser) {
+    Expr *left = parse_unary(parser);
+    while (parser->lookahead.type == TOKEN_STAR || parser->lookahead.type == TOKEN_SLASH) {
+        TokenType op = parser->lookahead.type;
+        match(parser, op);
+
+        Expr *right = parse_unary(parser);
+        left = ast_build_binary_expr_node(op, left, right);
+    }
+    
+    return left;
+}
+
 static Expr *parse_expr(Parser *parser) {
-    long num = parser->lookahead.num;
-    match(parser, TOKEN_NUMBER);
-    return ast_build_integer_expr_node(num);
+    Expr *left = parse_term(parser);
+    while (parser->lookahead.type == TOKEN_PLUS || parser->lookahead.type == TOKEN_MINUS) {
+        TokenType op = parser->lookahead.type;
+        match(parser, op);
+
+        Expr *right = parse_term(parser);
+        left = ast_build_binary_expr_node(op, left, right);
+    }
+
+    return left;
 }
 
 static ReturnStmt *parse_return_stmt(Parser *parser) {
@@ -70,9 +120,6 @@ static Function *parse_function(Parser *parser) {
 }
 
 Program *parse(Parser *parser) {
-    /*
-     * program -> function EOF
-     */
     Program *program = ast_build_program_node(parse_function(parser));
     match(parser, TOKEN_EOF);
     return program;
